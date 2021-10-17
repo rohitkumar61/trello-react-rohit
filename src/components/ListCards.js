7461cc7477fdca63871fd7a2c303da355331b6fe
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-18 02:28:41 +0530

    Add dropdown for delete card and its  functionality

--- src/components/ListCards.js
+++ src/components/ListCards.js
@@ -1,17 +1,49 @@
 import React, { Component } from "react";
 import { ListGroup } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import {
+  DropdownButton,
+  Dropdown,
+  InputGroup,
+  FormControl,
+} from "react-bootstrap";
 
 class ListCards extends React.Component {
   render() {
-let {id} =this.props
+    let { id } = this.props;
     return (
-      <div>
-        <ListGroup.Item className="m-2">{this.props.card.name}</ListGroup.Item>
+      <div style={{ position: "relative" }}>
+        <ListGroup.Item className="m-2">
+          <div  style={{margin:"13px"}} >
+            {this.props.card.name}
+            {/* <Button variant="outline-danger" onClick ={this.props.onDelete}>Danger</Button>{' '} */}
+          </div>
+          <DropdownButton
+            className="m-2"
+            variant=""
+            title=""
+            id="input-group-dropdown-2"
+            align="end"
+            ms-auto
+            style={{
+              right: "0px",
+              top: "0px",
+              textDecorationStyle: "none",
+              position: "absolute",
+              border: "none",
+              // backgroundColor: "white",
+            }}
+          >
+            <Dropdown.Item onClick={this.props.onDelete} href="#">
+              Delete Card
+            </Dropdown.Item>
+            <Dropdown.Item href="#">Another action</Dropdown.Item>
+            <Dropdown.Item href="#">Something else here</Dropdown.Item>
+          </DropdownButton>
+        </ListGroup.Item>
       </div>
     );
   }
 }
 
 export default ListCards;
-
-
