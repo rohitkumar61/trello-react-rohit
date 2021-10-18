4a6f08ed833c1292a6002f6c23628648df1e65fa
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-18 14:23:43 +0530

    Add modal on card for checklist

--- src/components/CheckListModalContainer.js
+++ src/components/CheckListModalContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Modal, Button } from "react-bootstrap";
 import { API_KEY, API_TOKEN } from "../services/projectServices";
 
+
+
 class CheckListModalContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -12,30 +14,69 @@ class CheckListModalContainer extends React.Component {
   }
 
   componentDidMount() {
-    let url = `https://api.trello.com/1/cards/${this.props.card.id}/checklists?key=${API_KEY}&token=${API_TOKEN}`;
+    //   console.log(this.props.cardData.id)
+    let url = `https://api.trello.com/1/cards/${this.props.cardData.id}/checklists?key=${API_KEY}&token=${API_TOKEN}`;
 
     fetch(url, {
       method: "GET",
     })
-	.then((response)=> response.json())
-	.then(res=>console.log(res))
+      .then((response) => response.json())
+      .then((cardCheckListData) => {
+        this.setState({ checkLists: cardCheckListData });
+      })
+
+      .catch((err) => console.log(err));
   }
+
+  handleShow = () => this.setState({ show: !this.state.show });
+
   render() {
     return (
       <>
-        <Button onClick={() => setLgShow(true)}>Large modal</Button>
+        <Button onClick={this.handleShow}>CheckList</Button>
+
         <Modal
           size="lg"
-          show={lgShow}
-          onHide={() => setLgShow(false)}
+          show={this.state.show}
+          onHide={this.handleShow}
           aria-labelledby="example-modal-sizes-title-lg"
         >
-          <Modal.Header closeButton>
-            <Modal.Title id="example-modal-sizes-title-lg">
-              Large Modal
+          {/* {this.state.checkLists.map((checklist) => { */}
+          {/* return( 	//   checklist={checklist}
+			//   key={checklist.id} */}
+
+          <Modal.Title id="example-modal-sizes-title-lg">
+            {this.props.cardData.name}
+          </Modal.Title>
+        
+         
+		  <Modal.Header closeButton>
+            <Modal.Title>
+              <form onSubmit={this.handleSubmit}>
+                <div className="mb-3">
+                  <label htmlFor="card-name" className="form-label">
+                    {/* create new board */}
+                  </label>
+                  <input
+                    value={this.state.name}
+                    type="text"
+                    className="form-control"
+                    id="card-name"
+                    aria-describedby="cardName"
+                    onChange={this.handleChange}
+                    placeholder="Create New Check List"
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  Add CheckList
+                </button>
+              </form>
             </Modal.Title>
           </Modal.Header>
-          <Modal.Body>...</Modal.Body>
+		 
+
+          <Modal.Body></Modal.Body>
         </Modal>
       </>
     );
