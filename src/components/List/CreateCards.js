be751641208d1929b41041fb5d56b813207d85ac
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-19 21:54:44 +0530

    remove console log

---
+++ src/components/List/CreateCards.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { ListGroup } from "react-bootstrap";
+
+class CreateCards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.onCreate(this.state.name);
+    this.setState({
+      name: "",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <ListGroup.Item className="m-4" style={{ minWidth: "200px" }}>
+          <form onSubmit={this.handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="card-name" className="form-label"></label>
+              <input
+                value={this.state.name}
+                type="text"
+                className="form-control"
+                id="card-name"
+                aria-describedby="cardName"
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary">
+              Add Cards
+            </button>
+          </form>
+        </ListGroup.Item>
+      </div>
+    );
+  }
+}
+
+export default CreateCards;
