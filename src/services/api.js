5a849a266673f8027d9fe1c3ab2bc49218f3f890
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-20 10:39:13 +0530

    remove encode uri method

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 export const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 export const API_KEY = process.env.REACT_APP_API_KEY;
 
-
-
 export function deleteCard(id) {
   let url = `https://api.trello.com/1/cards/${id}`;
   let meta = {
@@ -34,7 +32,7 @@ export function addBoard(name) {
 
 export function addList(boardId, name) {
   let url = `https://api.trello.com/1/lists`;
-  let queryParams = `&name=${encodeURI(name)}&idBoard=${boardId}`;
+  let queryParams = `&name=${name}&idBoard=${boardId}`;
   let meta = {
     method: "POST",
   };
@@ -44,7 +42,7 @@ export function addList(boardId, name) {
 
 export function addCard(id, name) {
   let url = `https://api.trello.com/1/cards`;
-  let queryParams = `&idList=${id}&name=${encodeURI(name)}`;
+  let queryParams = `&idList=${id}&name=${name}`;
 
   let meta = {
     method: "POST",
@@ -85,70 +83,65 @@ export function getCards(id) {
   return makeRequest(url, "", meta);
 }
 
-
 ///   checklist api
 
 export function updateCheckItem(cardId, checkItemId, state) {
-	state = state ? "complete" : "incomplete";
-  
-	let url = `https://api.trello.com/1/cards/${cardId}/checkItem/${checkItemId}`;
-	let queryParams = `&state=${state}`;
-	let meta = {
-	  method: "PUT",
-	};
-  
-	return makeRequest(url, queryParams, meta);
-  }
-  
-  export function deleteCheckItem(checkListId, CheckItemId) {
-	let url = `https://api.trello.com/1/checklists/${checkListId}/checkItems/${CheckItemId}`;
-	let meta = {
-	  method: "DELETE",
-	};
-  
-	return makeRequest(url, "", meta);
-  }
-  
-  export function addCheckItem(id, name) {
-	let url = `https://api.trello.com/1/checklists/${id}/checkItems`;
-	let queryParams = `&name=${name}`;
-	let meta = {
-	  method: "POST",
-	};
-  
-	return makeRequest(url, queryParams, meta);
-  }
-  
-  export function deleteChecklist(id) {
-	let url = `https://api.trello.com/1/checklists/${id}`;
-	let meta = {
-	  method: "DELETE",
-	};
-  
-	return makeRequest(url, "", meta);
-  }
-  
-  export function createChecklist(id, name) {
-	let url = `https://api.trello.com/1/cards/${id}/checklists`;
-	let queryParams = `&name=${encodeURI(name)}`;
-	let meta = {
-	  method: "POST",
-	};
-	return makeRequest(url, queryParams, meta);
-  }
-  
-  export function getCheckLists(id) {
-	let url = `https://api.trello.com/1/cards/${id}/checklists`;
-	let meta = {
-	  method: "GET",
-	};
-  
-	return makeRequest(url, "", meta);
-  }
-  
+  state = state ? "complete" : "incomplete";
 
+  let url = `https://api.trello.com/1/cards/${cardId}/checkItem/${checkItemId}`;
+  let queryParams = `&state=${state}`;
+  let meta = {
+    method: "PUT",
+  };
 
+  return makeRequest(url, queryParams, meta);
+}
 
+export function deleteCheckItem(checkListId, CheckItemId) {
+  let url = `https://api.trello.com/1/checklists/${checkListId}/checkItems/${CheckItemId}`;
+  let meta = {
+    method: "DELETE",
+  };
+
+  return makeRequest(url, "", meta);
+}
+
+export function addCheckItem(id, name) {
+  let url = `https://api.trello.com/1/checklists/${id}/checkItems`;
+  let queryParams = `&name=${name}`;
+  let meta = {
+    method: "POST",
+  };
+
+  return makeRequest(url, queryParams, meta);
+}
+
+export function deleteChecklist(id) {
+  let url = `https://api.trello.com/1/checklists/${id}`;
+  let meta = {
+    method: "DELETE",
+  };
+
+  return makeRequest(url, "", meta);
+}
+
+export function createChecklist(id, name) {
+  let url = `https://api.trello.com/1/cards/${id}/checklists`;
+  let queryParams = `&name=${name}`;
+  let meta = {
+    method: "POST",
+  };
+  return makeRequest(url, queryParams, meta);
+}
+
+export function getCheckLists(id) {
+  let url = `https://api.trello.com/1/cards/${id}/checklists`;
+  let meta = {
+    method: "GET",
+  };
+
+  return makeRequest(url, "", meta);
+}
 
 function makeRequest(url, queryParams, meta) {
   url += `?key=${API_KEY}&token=${API_TOKEN}${queryParams}`;
