5506dbdffd4c355cca189a5202e1d5453533aa0f
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-23 00:48:43 +0530

    Add switch case for add board

--- src/reducers/boardReducer.js
+++ src/reducers/boardReducer.js
@@ -1,17 +1,24 @@
-import { FETCH_BOARD } from "../actions/actionsTypes";
+import { FETCH_BOARD, ADD_BOARD } from "../actions/actionsTypes";
 
 const initialState = {
   boardsCard: [],
-//   board: {},
+  newBoard: {},
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case FETCH_BOARD:
-      console.log("reducer  working on case fetch match ....");
+      //   console.log("reducer  working on case fetch match ....");
       return {
         ...state,
         boardsCard: action.payload,
+      };
+
+    case ADD_BOARD:
+      //   console.log("reducer  working on case add card ....");
+      return {
+        ...state,
+        newBoard: action.payload,
       };
 
     default:
