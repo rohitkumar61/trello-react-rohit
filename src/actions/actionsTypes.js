e2d41c5951e74386efa6e68497a28bab5cd7ec51
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-23 12:24:29 +0530

    Add const label for fetch list

--- src/actions/actionsTypes.js
+++ src/actions/actionsTypes.js
@@ -1,2 +1,3 @@
-export const FETCH_BOARD = "FETCH_BOARD"
-export const ADD_BOARD = 'ADD_BOARD'
+export const FETCH_BOARD = "FETCH_BOARD";
+export const ADD_BOARD = "ADD_BOARD";
+export const FETCH_LISTS = "FETCH_LIST"
