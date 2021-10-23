a6763e0a54f0f3994f9d73df363757e6c1d09eca
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-23 15:17:23 +0530

    List archiev action

--- src/actions/listActions.js
+++ src/actions/listActions.js
@@ -1,5 +1,5 @@
 import * as TrelloApi from "./../services/api.js";
-import { FETCH_LISTS,ADD_LISTS} from "./actionsTypes";
+import { FETCH_LISTS,ADD_LISTS,ARCHIEVE_LIST} from "./actionsTypes";
 
 export const fetchListsAction = (id) => (dispatch) => {
 //   console.log("1 : ACTION ON LIST WORKING");
@@ -14,11 +14,21 @@ export const fetchListsAction = (id) => (dispatch) => {
 
 
 export const createListsAction = (id,newListName) => (dispatch) => {
-	console.log("crete listaction eorking")
+	
 	TrelloApi.addList(id, newListName).then((postList) =>
 	  dispatch({
 		type: ADD_LISTS,
 		payload: postList,
+	  })
+	);
+  };
+  
+
+  export const archieveListsAction = (listId) => (dispatch) => {
+  TrelloApi.archiveList(listId).then((archieveList) =>
+	  dispatch({
+		type: ARCHIEVE_LIST,
+		payload: archieveList.id,
 	  })
 	);
   };
