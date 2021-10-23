196577f4887b4de6f3c71b8c0cf74c7e05d89f13
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-23 16:00:35 +0530

    Add constants label for cards

--- src/actions/actionsTypes.js
+++ src/actions/actionsTypes.js
@@ -1,6 +1,13 @@
+//Action Types For Boards
 export const FETCH_BOARD = "FETCH_BOARD";
 export const ADD_BOARD = "ADD_BOARD";
 
+//Action Types For Lists
 export const FETCH_LISTS = "FETCH_LIST";
 export const ADD_LISTS = "ADD_LIST";
-export const ARCHIEVE_LIST ="ARCHIEVE_LIST"
+export const ARCHIEVE_LIST = "ARCHIEVE_LIST";
+
+//Action Types For Cards
+export const FETCH_CARD = "FETCH_CARD";
+export const ADD_CARD = "ADD_CARD";
+export const DELETE_CARD = "DELETE_CARD";
