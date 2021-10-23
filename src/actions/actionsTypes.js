fdeed14bfbeee6b7f6a87206662a1d50bfd6ea28
Rohit Kumar <rohit.kumar.17.1@mountblue.tech>
2021-10-23 15:16:27 +0530

    List const labels type

--- src/actions/actionsTypes.js
+++ src/actions/actionsTypes.js
@@ -1,3 +1,6 @@
 export const FETCH_BOARD = "FETCH_BOARD";
 export const ADD_BOARD = "ADD_BOARD";
-export const FETCH_LISTS = "FETCH_LIST"
+
+export const FETCH_LISTS = "FETCH_LIST";
+export const ADD_LISTS = "ADD_LIST";
+export const ARCHIEVE_LIST ="ARCHIEVE_LIST"
